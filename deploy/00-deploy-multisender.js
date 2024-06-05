50c20cc7287f3e8ff1357f9875d2ba80ec63cc97
Shivansh <shivansh0301@gmail.com>
2024-06-05 15:10:16 +0530

    minor fix

--- deploy/00-deploy-multisender.js
+++ deploy/00-deploy-multisender.js
@@ -1,11 +1,11 @@
 // Defining bytecode and abi from original contract on mainnet to ensure bytecode matches and it produces the same pair code hash
-const hre = require("hardhat");
+
 require("dotenv").config()
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy } = deployments;
-  const { deployer, proxyAdminOwner } = await getNamedAccounts();
-  const owner = deployer
+  const { deployer } = await getNamedAccounts();
+
 
   const Multisender = await deploy("Multisender", {
     from: deployer,
